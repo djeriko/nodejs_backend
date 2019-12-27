8d49c796ac6a099b78807807e5aab0387b764578
djeriko <jerikosalivan@gmail.com>
2019-12-27 19:15:35 +0900

    Lession 83

--- app.js
+++ app.js
@@ -5,17 +5,20 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRouter = require('./routes/admin');
-const shopRouter = require('./routes/shop');
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRouter);
-app.use(shopRouter);
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
 
 app.use((req,res, next)=> {
-    res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'));
+    res.status(404).render('404')
 });
 
 app.listen(3000);
