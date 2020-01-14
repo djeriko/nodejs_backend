8346f1b47e0cb60837cbf86f363b555ce0bd552e
djeriko <jerikosalivan@gmail.com>
2020-01-14 15:19:59 +0900

    test connection to db

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const errorController = require('./controllers/error');
+const errorController = require("./controllers/error");
+const db = require("./util/database");
 
 const app = express();
 
@@ -12,10 +13,18 @@ app.set("views", "views");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
+db.execute("SELECT * FROM products")
+  .then(result => {
+    console.log(result[0]);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost', 
     user: 'root',
     database: 'node-complete',
-    password: 'nodecomplete'
+    password: '1234567890'
 });
 
 module.exports = pool.promise();
